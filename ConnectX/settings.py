a9c8ad5a72eb39536c3f60ca35e0cd46f5654d1c
Md. Jahid Hasan <jahidhasanpiyesh@gmail.com>
2025-11-21 12:18:09 +0600

    Update regester base system and use is normal validations system

--- ConnectX/settings.py
+++ ConnectX/settings.py
@@ -96,18 +96,11 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'OPTIONS': {'min_length': 5}  # 5 digit password allow
     },
 ]
+
 
 
 # Internationalization
--- app1/forms.py
+++ app1/forms.py
@@ -3,36 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-    full_name = forms.CharField(
-        required=True,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Enter your full name',
-            'class': 'form-control'
-        })
-    )
-    email = forms.EmailField(
-        required=True,
-        widget=forms.EmailInput(attrs={
-            'placeholder': 'Enter your email',
-            'class': 'form-control'
-        })
-    )
-    password1 = forms.CharField(
-        required=True,
-        label='Password',
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Enter your password',
-            'class': 'form-control'
-        })
-    )
-    password2 = forms.CharField(
-        required=True,
-        label='Confirm Password',
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Confirm your password',
-            'class': 'form-control'
-        })
-    )
+    full_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
@@ -45,14 +17,12 @@ class RegisterForm(UserCreationForm):
         return email
 
     def save(self, commit=True):
-        user = super(RegisterForm, self).save(commit=False)
-        # Split full name into first_name and last_name
+        user = super().save(commit=False)
         full_name = self.cleaned_data['full_name'].strip().split(' ', 1)
         user.first_name = full_name[0]
         user.last_name = full_name[1] if len(full_name) > 1 else ''
         user.email = self.cleaned_data['email']
-        user.username = self.cleaned_data['email']  # Use email as username
-
+        user.username = self.cleaned_data['email']
         if commit:
             user.save()
         return user
